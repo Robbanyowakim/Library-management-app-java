784a20bdd4054c66f6cd8ed5e25a38b791c4e197
Robbanyowakim <Robertyowakim@gmail.com>
2018-11-21 12:58:23 +0100

    New class

--- LibraryManagement/src/com/company/Book.java
+++ LibraryManagement/src/com/company/Book.java
@@ -1,6 +1,21 @@
 package com.company;
 
-public class Book {
-    private String title;
-    private int age;
+import java.util.ArrayList;
+
+public class Book  {
+    private String ISBN;
+    private String name;
+    private ArrayList list;
+    private int numberOfPages;
+    private String language;
+    private String publisher;
+
+    public Book(String ISBN, String name, ArrayList list, int numberOfPages, String language, String publisher) {
+        this.ISBN = ISBN;
+        this.name = name;
+        this.list = list;
+        this.numberOfPages = numberOfPages;
+        this.language = language;
+        this.publisher = publisher;
+    }
 }
