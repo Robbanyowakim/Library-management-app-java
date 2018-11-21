c0ea24560ea32ccd277d83753c073804130882a0
Robbanyowakim <Robertyowakim@gmail.com>
2018-11-21 11:15:48 +0100

    Lagt till for loop

--- LibraryManagement/src/com/company/Main.java
+++ LibraryManagement/src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	for (int i = 0; i < 10; i++){
+        System.out.println( i + 5);
+        System.out.println("hello" + i);
+    }
     }
 }
