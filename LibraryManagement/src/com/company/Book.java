ac9998d5be93e2f731b5abec81cf8d887f8dda4f
Robbanyowakim <Robertyowakim@gmail.com>
2018-11-26 11:18:25 +0100

    Hej

--- LibraryManagement/src/com/company/Book.java
+++ LibraryManagement/src/com/company/Book.java
@@ -5,17 +5,17 @@ import java.util.ArrayList;
 public class Book  {
     private String ISBN;
     private String name;
-    private ArrayList list;
+    private ArrayList authors;
     private int numberOfPages;
     private String language;
     private String publisher;
 
-    public Book(String ISBN, String name, ArrayList list, int numberOfPages, String language, String publisher) {
+    public Book(String ISBN, String name, int numberOfPages, String language, String publisher, ArrayList authors) {
         this.ISBN = ISBN;
         this.name = name;
-        this.list = list;
         this.numberOfPages = numberOfPages;
         this.language = language;
         this.publisher = publisher;
+        this.authors = authors;
     }
 }
