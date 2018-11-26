68a9d35ce8500d33bd4aa19d91444753a62ffc63
Martin Malmberg <martiin98@gmail.com>
2018-11-26 11:37:01 +0100

    Added Class

---
+++ LibraryManagement/src/com/company/LibraryLogic.java
@@ -0,0 +1,5 @@
+package com.company;
+
+public class LibraryLogic {
+    //TODO ADD COMMENTS
+}
---
+++ LibraryManagement/src/com/company/Transaction.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Transaction {
+    private int transactionId;
+    private String bookId;
+    private Date dateOfIssue;
+    private Date dueDate;
+    private Date returnDate;
+    private double lateFee;
+
+}
