6e00edc1f52e131ab83b635ecca338ce67930f53
Robbanyowakim <Robertyowakim@gmail.com>
2018-11-29 11:24:38 +0100

    addBooks

--- LibraryManagement/src/com/company/LibraryLogic.java
+++ LibraryManagement/src/com/company/LibraryLogic.java
@@ -1,5 +1,13 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class LibraryLogic {
-    //TODO ADD COMMENTS
+    private ArrayList <Book> bookList = new ArrayList<>();
+
+
+    public Book addBooks (String ISBN,String name,int numberOfPages,String language, String publisher,ArrayList authorsList){
+        Book newBook = new Book(ISBN,name,numberOfPages,language,publisher,authorsList);
+        return newBook;
+    }
 }
--- LibraryManagement/src/com/company/Main.java
+++ LibraryManagement/src/com/company/Main.java
@@ -1,11 +1,48 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	for (int i = 0; i < 10; i++){
-        System.out.println( i + 5);
-        System.out.println("hello" + i);
+        Scanner input = new Scanner(System.in);
+        Main myApp = new Main();
+        LibraryLogic libraryLogic = new LibraryLogic();
+        myApp.menu();
+        int a = input.nextInt();
+        input.nextLine();
+        switch (a){
+            case 1:
+                System.out.print("Enter books ISBN number: ");
+                String ISBN = input.nextLine();
+                System.out.print("Enter the books name: ");
+                String name = input.nextLine();
+                System.out.print("Enter the number of pages: ");
+                int numberOfPages = input.nextInt();
+                input.nextLine();
+                System.out.print("Enter tha language of the book: ");
+                String language = input.nextLine();
+                System.out.print("Enter the publishers name: ");
+                String publisher = input.nextLine();
+                System.out.print("How many authors does the book have?");
+                int authors = input.nextInt();
+                input.nextLine();
+                ArrayList <String> authorsList = new ArrayList<>();
+                for (int i = 1; i <= authors; i++){
+                    System.out.printf("[%d] authors name: \n",i);
+                    String authorsName = input.nextLine();
+                    authorsList.add(authorsName);
+                }
+                libraryLogic.addBook(ISBN,name,numberOfPages,language,publisher,authorsList);
+
+
+        }
+
+
     }
+    private void menu (){
+        System.out.println(" --- --- --- --- ---");
+        System.out.println("| 1. Add Books      |");
     }
 }
