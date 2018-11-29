e3f3f0f18dd14ac465167c657de478dd9d3c3e43
Robbanyowakim <Robertyowakim@gmail.com>
2018-11-29 13:32:45 +0100

    do/while

--- Library/src/com/company/Main.java
+++ Library/src/com/company/Main.java
@@ -9,42 +9,45 @@ public class Main {
         Scanner input = new Scanner(System.in);
         Main myApp = new Main();
         LibraryLogic libraryLogic = new LibraryLogic();
-        myApp.menu();
-        int a = input.nextInt();
-        input.nextLine();
-        switch (a){
-            case 1:
-                System.out.print("Enter books ISBN number: ");
-                String ISBN = input.nextLine();
-                System.out.print("Enter the books name: ");
-                String name = input.nextLine();
-                System.out.print("Enter the number of pages: ");
-                int numberOfPages = input.nextInt();
-                input.nextLine();
-                System.out.print("Enter tha language of the book: ");
-                String language = input.nextLine();
-                System.out.print("Enter the publishers name: ");
-                String publisher = input.nextLine();
-                System.out.print("How many authors does the book have?");
-                int authors = input.nextInt();
-                input.nextLine();
-                ArrayList <String> authorsList = new ArrayList<>();
-                for (int i = 1; i <= authors; i++){
-                    System.out.printf("[%d] authors name: \n",i);
-                    String authorsName = input.nextLine();
-                    authorsList.add(authorsName);
-                }
-                libraryLogic.bookList.add(libraryLogic.addBooks(ISBN,name,numberOfPages,language,publisher,authorsList));
-            case 2:
-                for (int i = 0; i < libraryLogic.bookList.size(); i++){
-                    System.out.printf("[%d] %s",i,libraryLogic.bookList.get(i));
-                    // Jag är inte klar.
-                }
+        int loop = 0;
+        do {
+
+            myApp.menu();
+            int a = input.nextInt();
+            input.nextLine();
+            switch (a) {
+                case 1:
+                    System.out.print("Enter books ISBN number: ");
+                    String ISBN = input.nextLine();
+                    System.out.print("Enter the books name: ");
+                    String name = input.nextLine();
+                    System.out.print("Enter the number of pages: ");
+                    int numberOfPages = input.nextInt();
+                    input.nextLine();
+                    System.out.print("Enter tha language of the book: ");
+                    String language = input.nextLine();
+                    System.out.print("Enter the publishers name: ");
+                    String publisher = input.nextLine();
+                    System.out.print("How many authors does the book have?");
+                    int authors = input.nextInt();
+                    input.nextLine();
+                    ArrayList<String> authorsList = new ArrayList<>();
+                    for (int i = 1; i <= authors; i++) {
+                        System.out.printf("[%d] authors name: \n", i);
+                        String authorsName = input.nextLine();
+                        authorsList.add(authorsName);
+                    }
+                    libraryLogic.bookList.add(libraryLogic.addBooks(ISBN, name, numberOfPages, language, publisher, authorsList));
+                case 2:
+                    for (int i = 0; i < libraryLogic.bookList.size(); i++) {
+                        System.out.printf("[%d] %s", i, libraryLogic.bookList.get(i));
+                        // Jag är inte klar.
+                    }
 
 
-        }
+            }
 
-
+        } while (loop == 0);
     }
     private void menu (){
         System.out.println(" --- --- --- --- ---");
